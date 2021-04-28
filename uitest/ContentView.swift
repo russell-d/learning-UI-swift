1410c01e429047dc3fe76e7879820489eb2a4756
Russell De Vera <rdevera@csumb.edu>
2021-04-27 18:56:35 -0700

    Initial Commit

---
+++ uitest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by Russell De Vera on 4/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Russell De Vera on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
